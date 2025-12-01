98b382d600f89f92e5b82968c51b233d2491b616
unknown <you@example.com>
2025-12-01 23:48:43 +0600

    2,12,25

---
+++ Car-Rental/inc/navbar.php
@@ -0,0 +1,34 @@
+<header class="header-main">
+			<!-- Logo Block -->
+			<div class="middle-header container-fluid no-padding">
+				<!-- Container -->
+				<div class="container">
+					<div class="col-md-4 logo-block pull-left">
+						<a href="index.php">
+							<img src="images/logo.png" alt="Logo" />
+							<b>National Cab</b><span>Always there for you</span>
+						</a>
+					</div>
+					<div class="col-md-8 pull-right">
+						<nav class="navbar ow-navigation">
+							<div class="container">
+
+								<div id="navbar" class="navbar-collapse collapse">
+									<ul class="nav navbar-nav">
+										<li><a href="index.php">Home </a></li>
+										<li><a href="about.php">About</a></li>
+										<li><a href="service.php">Services</a></li>
+										<li><a href="contact_us.php">Contact</a></li>
+										<li><a href="login.php">Login</a></li>
+										<li><a href="online_booking.php">Book Now</a></li>
+									</ul>
+								</div>
+
+							</div>
+						</nav>
+
+					</div>
+				</div><!-- Container /- -->
+			</div><!-- Logo Block /- -->
+
+		</header>
---
+++ Car-Rental/index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html class=""><!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>National Cab - Taxi Company HTML5 Template</title>
+
+	<!-- Standard Favicon -->
+	<link rel="icon" type="image/x-icon" href="images//favicon.ico" />
+
+	<!-- For iPhone 4 Retina display: -->
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images//apple-touch-icon-114x114-precomposed.png">
+
+	<!-- For iPad: -->
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images//apple-touch-icon-72x72-precomposed.png">
+
+	<!-- For iPhone: -->
+	<link rel="apple-touch-icon-precomposed" href="images//apple-touch-icon-57x57-precomposed.png">
+
+	<!-- Library - Loader CSS -->
+	<link rel="stylesheet" type="text/css" href="libraries/loader/loaders.min.css">
+
+	<!-- Library - Google Font Familys -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Cabin:400,400italic,500,500italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,100italic,100,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+	<!-- Library - Bootstrap v3.3.5 -->
+	<link rel="stylesheet" type="text/css" href="libraries/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="libraries/bootstrap/bootstrap-datetimepicker.min.css">
+
+	<!-- Font Icons -->
+	<link rel="stylesheet" type="text/css" href="libraries/fonts/font-awesome.min.css">
+
+	<!-- Library - OWL Carousel V.2.0 beta -->
+	<link rel="stylesheet" type="text/css" href="libraries/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" type="text/css" href="libraries/owl-carousel/owl.theme.css">
+
+	<!-- Library - FlexSlider v2.5.0 -->
+	<link rel="stylesheet" type="text/css" href="libraries/flexslider/flexslider.css">
+
+	<!-- Library - Animate CSS -->
+	<link rel="stylesheet" type="text/css" href="libraries/animate/animate.min.css">
+
+	<!-- Custom - Common CSS -->
+	<link rel="stylesheet" type="text/css" href="css/plugins.css">
+	<link rel="stylesheet" type="text/css" href="css/navigation-menu.css">
+
+	<!-- Custom - Theme CSS -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/shortcodes.css">
+
+	<!--[if lt IE 9]>
+		<script src="js/html5/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body data-offset="200" data-spy="scroll" data-target=".ow-navigation">
+	<!-- Loader -->
+	<div id="site-loader" class="load-complete">
+		<div class="loader">
+			<div class="loader-inner ball-clip-rotate">
+				<div></div>
+			</div>
+		</div>
+	</div><!-- Loader /- -->
+
+	<a id="top"></a>
+	<!-- Main Container -->
+	<div class="main-container">
+		<!-- Header -->
+		<?php include("inc/navbar.php"); ?>
+		<!-- Header /- -->
+			<!-- Photo Slider -->	
+	<div class="container-fluid photos-slider no-padding">
+		<div id="photos-slider" class="carousel slide" data-ride="carousel">			
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+					<img src="images/photos-slider/photo-slide-1.jpg" alt="side-1"/>
+					<div class="carousel-caption">
+						<h2>A reliable way to travel</h2>
+						<p>Come and listen to a story about a man named Jed a poor mountaineer barely kept his family fed so lets make the most of this beautiful day since we are together it is a beautiful day in this neighborhood a beautiful day for a neighbor</p>
+						<a href="#" class="purchase">Purchase</a>
+						<a href="#" title="See More" class="see-more">See More</a>						
+					</div>
+				</div>
+				<div class="item">
+					<img src="images/photos-slider/photo-slide-1.jpg" alt="side-2"/>
+					<div class="carousel-caption">
+						<h2>A reliable way to travel</h2>
+						<p>Come and listen to a story about a man named Jed a poor mountaineer barely kept his family fed so lets make the most of this beautiful day since we are together it is a beautiful day in this neighborhood a beautiful day for a neighbor</p>
+						<a href="#" class="purchase">Purchase</a>
+						<a href="#" class="see-more">See More</a>						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div ><!-- Photo Slider/- -->
+	
+	<!-- Choose Us -->
+	<div class="container-fluid no-padding choose-us">
+		<div class="section-padding"></div>
+		<!-- Container -->
+		<div class="container">
+			<div class="section-header">
+				<h3>Reasons To Choose Us</h3>				
+			</div>
+			<!-- Choose Carousel -->
+			<div class="choose-carousel">			
+				<div class="col-md-12">
+					<div class="choose-us-box">
+						<div class="choose-img-box"><img src="images/choose-us/choose-us-1.jpg" alt="choose-us-1"/></div>
+						<div class="choose-us-content-box">
+							<h3>Trust And Safety</h3>
+							<p>The first mate and his Skipper too will do their very best to make the others in their tropic island nest the year.</p>
+							<a href="#">Read More</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-12">
+					<div class="choose-us-box">
+						<div class="choose-img-box"><img src="images/choose-us/choose-us-2.jpg" alt="choose-us-2"/></div>
+						<div class="choose-us-content-box">
+							<h3>Feel The Comfort</h3>
+							<p>The first mate and his Skipper too will do their very best to make the others in their tropic island nest the year.</p>
+							<a href="#">Read More</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-12">
+					<div class="choose-us-box">
+						<div class="choose-img-box"><img src="images/choose-us/choose-us-3.jpg" alt="choose-us-3"/></div>
+						<div class="choose-us-content-box">
+							<h3>Experienced Drivers</h3>
+							<p>The first mate and his Skipper too will do their very best to make the others in their tropic island nest the year.</p>
+							<a href="#">Read More</a>
+						</div>
+					</div>
+				</div>
+			</div><!-- Choose Carousel/- -->
+		</div><!-- Container/- -->
+		<div class="section-padding"></div>
+	</div><!-- Choose Us/- -->
+	
+	<!-- Book Details -->
+	<div class="container-fluid no-padding book-details">
+		<div class="section-padding"></div>
+		<!-- Container -->
+		<div class="container">
+			<div class="section-header">
+				<h3>Book For a Ride</h3>				
+			</div>
+			<!-- Booking Details Form -->
+			<form class="booking-details-form">
+				<div class="form-group col-md-3 col-sm-4 col-xs-12">
+					<label>Name</label>
+					<input type="text" class="form-control" id="name" placeholder="Enter Your Name Here">
+				</div>
+				<div class="form-group col-md-3 col-sm-4 col-xs-12">
+					<label>Phone Number</label>
+					<input type="text" class="form-control" id="phone" placeholder="+(01) xxx - xxx - xx">
+				</div>
+				<div class="form-group col-md-3 col-sm-4 col-xs-12">
+					<label>From</label>
+					<input type="text" class="form-control" id="from" placeholder="Starting Place">
+				</div>
+				<div class="form-group col-md-3 col-sm-4 col-xs-12">
+					<label>To</label>
+					<input type="text" class="form-control" id="destination" placeholder="Destination">
+				</div>
+				<div class="form-group col-md-4 col-sm-8 col-xs-12">
+					<label>Date &amp; Time</label>
+					<div class="row date-time">
+						<div class="col-md-6 col-sm-6 col-xs-6"><input type="text" class="form-control" id="date_time" placeholder="DD/MM/YYYY"></div>
+						<div class="col-md-3 col-sm-3 col-xs-3"><input type="text" class="form-control" id="hours" placeholder="HH"></div>
+						<div class="col-md-3 col-sm-3 col-xs-3"><input type="text" class="form-control" id="month" placeholder="MM"></div>
+					</div>
+				</div>
+				<div class="form-group col-md-2 col-sm-4 col-xs-12">
+					<label>Passenger</label>
+					<input type="number" class="form-control" id="passenger" placeholder="4">
+				</div>
+				<div class="form-group col-md-3 col-sm-4 col-xs-12">
+					<label>Class</label>
+					<select class="form-control" id="class">
+						<option>Business</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+					</select>
+				</div>
+				<div class="form-group col-md-3 col-sm-4 col-xs-12">
+					<button type="submit" class="btn">Book Your Trip</button>
+				</div>
+			</form><!-- Booking Details Form/- -->
+		</div><!-- Container/- -->
+		<div class="section-padding"></div>
+	</div><!-- Book Details/- -->
+	
+	<!-- What We Do -->
+	<div class="container-fluid no-padding what-we-do">
+		<div class="section-padding"></div>
+		<div class="image-box">
+			<img src="images/services/service-1.png" alt="service" />
+		</div>
+		<!-- Container -->
+		<div class="container">			
+			<div class="col-md-8 pull-right">
+				<div class="we-do-more">
+					<h3>We Do More</h3>
+					<h2>Than You Wish</h2>
+					<p>The mate was a mighty sailin man the Skipper brave and sure. Five passengers set sail that day for a three hour tour a three hour tour.</p>
+				</div>
+				<div class="col-md-6">
+					<div class="what-we-do-box">						
+						<span>Home Pickup</span>
+						<img src="images/icon/home-pickup.png" alt="home-pickup"/>
+						<p>Come and dance on our floor. Take a step that is new. We've a loveable space that needs your face threes company too.</p>
+						<a href="#">Read More</a>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="what-we-do-box">						
+						<span>Airport Transportation</span>
+						<img src="images/icon/airport.png" alt="airport"/>
+						<p>Come and dance on our floor. Take a step that is new. We've a loveable space that needs your face threes company too.</p>
+						<a href="#">Read More</a>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="what-we-do-box">						
+						<span>Company Contract</span>
+						<img src="images/icon/company-contract.png" alt="company-contract"/>
+						<p>Got a dream and we just know now we are gonna make us dream come and we know flipper lives in a world </p>
+						<a href="#">Read More</a>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="what-we-do-box">
+						<p>No one you see is smarter than he. The mate was a mighty sailin man Skip brave and sure Five</p>
+						<div class="more-service">
+							<a href="#">More Service</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!-- Container/-  -->
+		<div class="section-padding"></div>
+	</div><!-- What We Do/- -->
+
+
+
+		<!-- <footer>-->
+		<?php include("inc/footer.php") ; ?>
+		<!--</footer> -->
+
+	</div><!-- Main Container -->
+	<!-- JQuery v1.11.3 -->
+	<script src="js/jquery.min.js"></script>
+
+	<!-- Library - Modernizer -->
+	<script src="libraries/modernizr/modernizr.js"></script>
+
+	<!-- Library - Bootstrap v3.3.5 -->
+	<script src="libraries/bootstrap/bootstrap.min.js"></script><!-- Bootstrap JS File v3.3.5 -->
+	<script src="libraries/bootstrap/bootstrap-datetimepicker.min.js"></script><!-- Bootstrap JS File v3.3.5 -->
+
+	<!-- jQuery Easing v1.3 -->
+	<script src="js/jquery.easing.min.js"></script>
+
+	<!-- Library - jQuery.appear -->
+	<script src="libraries/appear/jquery.appear.js"></script>
+
+	<!-- Library - OWL Carousel V.2.0 beta -->
+	<script src="libraries/owl-carousel/owl.carousel.min.js"></script>
+
+	<!-- jQuery For Number Counter -->
+	<script src="libraries/number/jquery.animateNumber.min.js"></script>
+
+	<!-- Library - Google Map API -->
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+
+	<!-- Library - FlexSlider v2.5.0 -->
+	<script defer src="libraries/flexslider/jquery.flexslider.js"></script>
+
+	<!-- Library - Theme JS -->
+	<script src="js/functions.js"></script>
+</body>
+
+</html>
