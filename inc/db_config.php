a1eb3c59b620ae1b7cf3da411f308c37b6f08ce9
unknown <you@example.com>
2025-12-05 22:46:56 +0600

    nai

--- inc/db_config.php
+++ inc/db_config.php
@@ -2,7 +2,7 @@
  $host = "localhost";
  $user = "root";
  $password = "";
- $database = "car_rental";
+ $database = "carrental";
  $db = new mysqli($host, $user, $password, $database);
 //  if ($db->connect_error) {
 //     die("Connection failed: " . $db->connect_error);
