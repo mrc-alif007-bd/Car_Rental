d6f3153fb59e2463af135d814333c53750c537f8
mrc-alif007-bd <mrc.alif007@gmail.com>
2025-12-10 12:54:16 +0600

    ok

--- admin/inc/left_bar.php
+++ admin/inc/left_bar.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?php echo $admin_url ?>dashboard.php" class="brand-link">
-        <img src="<?php echo $admin_url ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?php echo $admin_url ?>admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Dashboard</span>
     </a>
 
@@ -75,9 +75,9 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="pages/tables/data.html" class="nav-link">
+                            <a href="employees.php" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>DataTables</p>
+                                <p>Employees</p>
                             </a>
                         </li>
                         <li class="nav-item">
